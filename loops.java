b109e4af756c362d3a227e4d53b1cb2cf09044e5
Sam Cardin <blucardin@gmail.com>
2022-05-25 17:12:55 -0400

    add code

--- loops.java
+++ loops.java
@@ -1,7 +1,15 @@
 public class loops {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Hello World!");
+        String name = "noah";
+
+        for (int i = 0; i < name.length(); i++) {
+            for (int q = 0; q < name.length(); q++) {
+                for (int w = 0; w < name.length(); w++) {
+                    for (int e = 0; e < name.length(); e++) {
+                        System.out.println("" + name.charAt(i) + name.charAt(q) + name.charAt(w) + name.charAt(e));
+                    }
+                }
+            }
         }
     }
 }
